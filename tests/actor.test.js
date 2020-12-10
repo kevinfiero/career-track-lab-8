9a1f2c1ef3e0f1c551a36bab81939b2b21f72933
kevinfiero <kjf46@cornell.edu>
2020-12-10 15:41:04 -0800

    get blank test to pass

---
+++ tests/actor.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const app = require('../app');
+const pool = require('../lib/utils/pool');
+const Actor = require('../lib/models/actor');
+const Movie = require('../lib/models/movie');
+
+describe('test actor model', () => {
+
+  it('insert actor', async() => {
+
+  });
+
+});
---
+++ tests/movie.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const app = require('../app');
+const pool = require('../lib/utils/pool');
+const Actor = require('../lib/models/actor');
+const Movie = require('../lib/models/movie');
+
+describe('test movie model', () => {
+
+  it('insert movie', async() => {
+
+  });
+
+});
