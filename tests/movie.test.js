8e4f781281ce766df6e3ec47ae189c01234d31f7
kevinfiero <kjf46@cornell.edu>
2020-12-10 15:53:46 -0800

    added sql

--- tests/movie.test.js
+++ tests/movie.test.js
@@ -1,10 +1,19 @@
 const request = require('supertest');
 const app = require('../app');
 const pool = require('../lib/utils/pool');
+const fs = require('fs');
 const Actor = require('../lib/models/actor');
 const Movie = require('../lib/models/movie');
 
 describe('test movie model', () => {
+
+  beforeEach(() => {
+    return pool.query(fs.readFileSync('./lib/sql/setup.sql', 'utf-8'));
+  });
+
+  afterAll(() => {
+    return pool.end();
+  });
 
   it('insert movie', async() => {
 
